18b87d376d06202085d743a7bc38c9bf49ad45ff
flyingwithgm <georgenuyekpe@gmail.com>
2025-08-14 10:34:07 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Kl8otpakwNYDAM88YlTJ71OnM_Qb-Sk",
-  authDomain: "studybuddyghana.firebaseapp.com",
-  projectId: "studybuddyghana",
-  storageBucket: "studybuddyghana.firebasestorage.app",
-  messagingSenderId: "775379484331",
-  appId: "1:775379484331:web:65443b3d223772e220c1ee"
+    apiKey: "AIzaSyC2Kl8otpakwNYDAM88YlTJ71OnM_Qb-Sk",
+    authDomain: "studybuddyghana.firebaseapp.com",
+    projectId: "studybuddyghana",
+    storageBucket: "studybuddyghana.firebasestorage.app",
+    messagingSenderId: "775379484331",
+    appId: "1:775379484331:web:65443b3d223772e220c1ee"
 };
 
 export default firebaseConfig;
